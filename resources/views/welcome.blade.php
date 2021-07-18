d0f1ef2772745b27d931a945da7eb66932a86115
DiegoEmanuel <diegoabox@gmail.com>
2021-07-18 01:47:19 -0300

    Design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,25 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Api do Ajuai com Laravel</title>
-        <ul>
-            <li>index – Lista os dados da tabela</li>
-            <li>show – Mostra um item específco</li>
-            <li>create – Retorna a View para criar um item da tabela</li>
-            <li>edit – Retorna a View para edição do dado</li>
-            <li>store – Salva o novo item na tabela</li>
-            <li>destroy – Remove o dado</li>
-            <li>update – Salva a atualização do dado</li>
-        </ul>
-        donor
-        <p>address</p>
-            <p>cities</p>
-                <p>donations</p>
-                    <p>donations_items</p>
-                        <p>associations</p>
-                            <p>campaigns</p>
-                                <p>projects</p>
-                                    <p>social</p>
+
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
@@ -68,6 +50,25 @@
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
                                 <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Documentation</a></div>
+                                <title>Api do Ajuai com Laravel</title>
+        <ul>
+            <li>index – Lista os dados da tabela</li>
+            <li>show – Mostra um item específco</li>
+            <li>create – Retorna a View para criar um item da tabela</li>
+            <li>edit – Retorna a View para edição do dado</li>
+            <li>store – Salva o novo item na tabela</li>
+            <li>destroy – Remove o dado</li>
+            <li>update – Salva a atualização do dado</li>
+        </ul>
+        donor
+        <p>address</p>
+            <p>cities</p>
+                <p>donations</p>
+                    <p>donations_items</p>
+                        <p>associations</p>
+                            <p>campaigns</p>
+                                <p>projects</p>
+                                    <p>social</p>
                             </div>
                             <img src="https://raw.githubusercontent.com/DiegoEmanuel/backend-ajudai/master/preview.png" width="700px" alt="Preview">
                         </div>
